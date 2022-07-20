a040c45a2d1f06c8bb1dbd20cc2ca216816f3698
yana sarvarova <serg.yana@mail.ru>
2022-07-20 14:10:43 +0300

    Задача 54

---
+++ Task01/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задайте двумерный массив. Напишите программу,
+ которая упорядочит по убыванию элементы каждой строки двумерного массива. */
+
+Task54();
+
+void Task54()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 10);
+    int columns = random.Next(4, 10);
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    Console.WriteLine("Вывод массива");
+    PrintArray(array);
+    Console.WriteLine("Упорядоченный массив");
+    OrderedArray(array);
+    PrintArray(array);
+}
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(-50, 51);
+        }
+    }
+
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderedArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns - 1; j++)
+        {
+            for (int k = j + 1; k < columns; k++)
+            if (array [i, k] < array [i, j]) (array[i, j], array[i, k]) = (array[i,k], array [i,j]);
+        }
+
+    }
+}
